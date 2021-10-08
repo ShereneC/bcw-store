6b78ac01e03eb33c8f7fe57d47a0a1cfc1cdcad6
ShereneC <spyhopper@cableone.net>
2021-10-08 11:24:01 -0600

    Db connected

--- server/db/dbconfig.js
+++ server/db/dbconfig.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 
 // said he needed these things for his mongoose/atlas configuation, and we may want them.
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.connection.on('error', err => {
-  console.error('[DATABASE ERROR]:', err)
-})
+// mongoose.set('useNewUrlParser', true);
+// mongoose.set('useFindAndModify', false);
+// mongoose.set('useCreateIndex', true);
+// mongoose.connection.on('error', err => {
+//   console.error('[DATABASE ERROR]:', err)
+// })
 
-const connectionString = process.env.connectionString || ""
+const connectionString = process.env.connectionString || "mongodb+srv://Student:SuperSecret123@classroom.qiutj.mongodb.net/BCWStore?retryWrites=true&w=majority"
 
 export default class DbContext {
   static async connect() {
